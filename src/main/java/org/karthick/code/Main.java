e1ac39326c940f2da751ce7f4b81a9276120392d
Karthick <karthick1110@gmail.com>
2022-11-10 04:09:28 +0530

    Added dependencies json.

---
+++ src/main/java/org/karthick/code/Main.java
@@ -0,0 +1,16 @@
+package org.karthick.code;
+
+import org.json.simple.parser.JSONParser;
+
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+
+public class Main {
+    public static void main(String[] args) throws FileNotFoundException {
+        //JSON parser object to parse read file
+        JSONParser jsonParser = new JSONParser();
+        FileReader compare_1 =  new FileReader("compareFirst.json");
+
+
+    }
+}
